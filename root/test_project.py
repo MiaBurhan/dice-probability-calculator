b47032d0ea4d09968d0f5bbd6556e697bcb3c135
Burhan <bmia2684@gmail.com>
2026-06-14 22:03:47 +0500

    Testing in a different way.
    This commit tests the program in a fractional way which is more precise and compatible.

--- root/test_project.py
+++ root/test_project.py
@@ -1,40 +1,31 @@
-from dice_probability import calculate_sum_probability, at_least_success, at_most_success
+from Project import calculate_sum_probability, at_least_success, at_most_success
 
 import pytest
 
-# Test of the function sum_probability for one dice
-def sum_probability():
-    p_once, p_atleast = calculate_sum_probability(1, 1, 3)
-    assert p_once == pytest.approx(1/6)
-    assert p_atleast == pytest.approx(1/6)
+def test_sum_ratioability():
+    fraction,ratio = calculate_sum_probability(3, 3)
+    assert ratio == pytest.approx(1/216)
 
-# Test at_least_success function
 def test_at_least_success():
-    # 1 die, threshold 4 (success if sum >=4, p=3/6=0.5)
-    # 1 throw, at least 1 success: 0.5
-    prob = at_least_success(1, 1, 4, 1)
-    assert prob == pytest.approx(0.5)
+  
+    ratio = at_least_success(1, 1, 4, 1)
+    assert ratio == pytest.approx(1/6)
     
-    # 2 throws, at least 1 success: 1 - (0.5)^2 = 0.75
-    prob = at_least_success(1, 2, 4, 1)
-    assert prob == pytest.approx(0.75)
     
-    # 2 throws, at least 2 successes: (0.5)^2 = 0.25
-    prob = at_least_success(1, 2, 4, 2)
-    assert prob == pytest.approx(0.25)
+    ratio = at_least_success(1, 2, 4, 1)
+    assert ratio == pytest.approx(11/36)
+    
+    ratio= at_least_success(1, 2, 4, 2)
+    assert ratio == pytest.approx(1/36)
 
-# Test at_most_success function  
 def test_at_most_success():
-    # 1 die, threshold 4, p=0.5
-    # 1 throw, at most 0 successes: 0.5
-    prob = at_most_success(1, 1, 4, 0)
-    assert prob == pytest.approx(0.5)
     
-    # 2 throws, at most 1 success: (0.5)^2 + 2*(0.5)*(0.5) = 0.25 + 0.5 = 0.75
-    prob = at_most_success(1, 2, 4, 1)
-    assert prob == pytest.approx(0.75)
+    ratio = at_most_success(1, 1, 4, 0)
+    assert ratio == pytest.approx(5/6)
     
-    # 2 throws, at most 2 successes: 1.0
-    prob = at_most_success(1, 2, 4, 2)
-    assert prob == pytest.approx(1.0)
+    ratio = at_most_success(1, 2, 4, 1)
+    assert ratio == pytest.approx(35/36)
+    
+    ratio = at_most_success(1, 2, 4, 2)
+    assert ratio == pytest.approx(1)
     
